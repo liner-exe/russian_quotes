ef8c857ffb5545c9d9b749d45f24957a2c94aba2
r-liner <romamolibog@gmail.com>
2024-02-21 19:24:11 +0300

    v2.0

--- russian_quotes/__init__.py
+++ russian_quotes/__init__.py
@@ -1,5 +1,51 @@
-from .quotes import get_quote
-from .quotes_async import get_quote_coro
-from .exceptions import ServerError
+"""
+Russian Quotes
+~~~~~~~~~~~~~~
 
-__version__ = '1.7'
+Quotes of famous people on russian. Async & Sync.
+
+Unofficial Forismatic API Wrapper.
+
+&copy; r-liner 2023-present
+:license: MIT
+
+## Examples
+
+### Sync
+
+```py
+from russian_quotes import get_quote
+
+quote_author, quote_text = get_quote()
+
+print(f'{quote_author}-{quote_text}')
+
+>>> The undertaking of a new action brings new strength. -Richard Evans
+```
+
+### Async
+
+```py
+from russian_quotes import get_quote_async
+import asyncio
+
+quote_author, quote_text = asyncio.run(get_quote_async())
+
+print(f'{quote_author}-{quote_text}')
+
+>>> Every action of our lives touches on some chord that will vibrate in eternity. -Edwin Chapin
+```
+"""
+
+from .quotes import get_quote, get_quote_async
+from .exceptions import ServerError, LanguageIsNotSupported
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __license__,
+    __title__,
+    __description__,
+    __url__,
+    __version__,
+)
