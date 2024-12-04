d36fea718361b2d77d5b3814f2e3d366ebc0f4af
Molibog Roman <90984207+liner-exe@users.noreply.github.com>
2024-12-04 16:48:28 +0300

    New import (v2.2.0)

--- russian_quotes/__init__.py
+++ russian_quotes/__init__.py
@@ -38,7 +38,11 @@ print(f'{quote_author}-{quote_text}')
 """
 
 from .quotes import get_quote, get_quote_async
-from .exceptions import ServerError, LanguageIsNotSupported
+from .exceptions import (
+    ServerError,
+    LanguageIsNotSupported,
+    QuoteKeyError
+)
 from .languages import Languages
 from .__version__ import (
     __author__,
