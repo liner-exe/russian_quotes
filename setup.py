f909e34fd207c95b039d0f37403a30f13d10d492
Molibog Roman <90984207+liner-exe@users.noreply.github.com>
2024-12-05 22:39:54 +0300

    Version bumped (v2.2.0)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
 
 setup(
     name='russian_quotes',
-    version='2.1.0',
+    version='2.2.0',
     author='liner',
     author_email='contact.liner999@gmail.com',
     description='Quotes of famous people in Russian & English',
